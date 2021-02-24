f7f6d662525776db86599eaf5c500dad02c418ed
Nitin <nitin.khandelwal@tothenew.com>
2021-02-24 22:46:23 +0530

    mavenAssignment

---
+++ MavenPro/src/main/java/myPack/App.java
@@ -0,0 +1,6 @@
+package myPack;
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ MavenPro/src/test/java/myPack/AppTest.java
@@ -0,0 +1,7 @@
+package myPack;
+
+public class AppTest {
+    public static void main(String[] args) {
+        System.out.println("Testing............");
+    }
+}
